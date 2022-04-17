cf257564321356ac74c83a254ec0d93e3b565b6e
Dmitry Zakharov <83185476+sasukeuchiwa666@users.noreply.github.com>
2022-04-17 21:44:39 +0300

    День 14 Начал разработку компонента программы митапа

--- src/components/meetupPage/MeetupAgenda.vue
+++ src/components/meetupPage/MeetupAgenda.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="meetup-agenda-wrapper">
     <h4>Программа:</h4>
-    <div class="meetup-agenda-wrapper-item"></div>
+    <div
+      class="meetup-agenda-wrapper-item"
+      v-for="item in agenda"
+      :key="item.id"
+    >
+      <MeetupAgendaItem :agenda-item="item" />
+    </div>
   </div>
 </template>
 <script>
+import MeetupAgendaItem from '@/components/meetupPage/MeetupAgendaItem';
 export default {
   name: 'MeetupAgenda',
-  components: {},
-  data: () => ({}),
+  components: { MeetupAgendaItem },
+  computed: {
+    agenda() {
+      return this.$store.getters.meetup.agenda;
+    },
+  },
 };
 </script>
 <style scoped lang="scss"></style>
---
+++ src/components/meetupPage/MeetupAgendaItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="agenda-item-wrapper">
+    <div class="agenda-item-wrapper-icon">
+      <UiIcon class="agenda-item-icon" :icon-name="agendaItemIconName" />
+    </div>
+    <div class="agenda-item-wrapper-info"></div>
+  </div>
+</template>
+<script>
+import UiIcon from '@/components/ui/UiIcon';
+import { agendaItemIcons } from '@/services/_agenda.service';
+
+export default {
+  name: 'MeetupAgendaItem',
+  components: { UiIcon },
+  props: {
+    agendaItem: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  computed: {
+    agendaItemIconName() {
+      return agendaItemIcons[this.agendaItem.type];
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+.agenda-item-wrapper {
+  .agenda-item-wrapper-icon {
+    display: flex;
+    margin-top: 25px;
+    .agenda-item-icon {
+      width: 40px;
+    }
+  }
+}
+</style>
